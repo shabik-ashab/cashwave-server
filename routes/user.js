961b4bff5173dcc489f6d108b114e64bebac86a3
nub69 <shabik.ashab@gmail.com>
2024-03-09 16:31:22 +0600

    feat(routes): signin route added

--- routes/user.js
+++ routes/user.js
@@ -13,6 +13,11 @@ const signupBody = zod.object({
 	password: zod.string()
 })
 
+const signinBody = zod.object({
+    username: zod.string().email(),
+	password: zod.string()
+})
+
 router.post("/signup", async (req, res) => {
     const { success } = signupBody.safeParse(req.body)
     if (!success) {
@@ -46,6 +51,36 @@ router.post("/signup", async (req, res) => {
     res.json({
         message: "User created successfully",
         token: token
+    })
+})
+
+router.post("/signin", async (req, res) => {
+    const { success } = signinBody.safeParse(req.body)
+    if (!success) {
+        return res.status(411).json({
+            message: "Incorrect inputs"
+        })
+    }
+
+    const user = await User.findOne({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    if (user) {
+        const token = jwt.sign({
+            userId: user._id
+        }, JWT_SECRET);
+  
+        res.json({
+            token: token
+        })
+        return;
+    }
+
+    
+    res.status(411).json({
+        message: "Error while logging in"
     })
 })
 
