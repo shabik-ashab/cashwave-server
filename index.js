52d52cdfa7a742901caed3269904ea7452642f27
nub69 <shabik.ashab@gmail.com>
2024-03-09 13:01:23 +0600

    feat: intialized jwt

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const cors = require("cors")
 const mongoose = require('mongoose');
 const rootRouter = require("./routes/index");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+app.use(cors())
+app.use(express.json())
 
 app.use("api/v1", rootRouter)
 
