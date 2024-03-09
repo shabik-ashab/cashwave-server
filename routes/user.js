f9bf638794036b421ef99b3cbeeb583e5126b74c
nub69 <shabik.ashab@gmail.com>
2024-03-09 16:58:15 +0600

    feat(routes): get users, filterable via firstName/lastName

--- routes/user.js
+++ routes/user.js
@@ -106,4 +106,29 @@ router.put("/", authMiddleware, async (req, res) => {
     })
 })
 
+router.get("/bulk", async (req, res) => {
+    const filter = req.query.filter || "";
+
+    const users = await User.find({
+        $or: [{
+            firstName: {
+                "$regex": filter
+            }
+        }, {
+            lastName: {
+                "$regex": filter
+            }
+        }]
+    })
+
+    res.json({
+        user: users.map(user => ({
+            username: user.username,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            _id: user._id
+        }))
+    })
+})
+
 module.exports = router;
